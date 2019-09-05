3c0c0188c4e44b20559a45c848638fa51bd0e884
Marco Ambrosini <marcoambrosini@pm.me>
2019-09-05 10:39:12 +0200

    Update to use the refactored AppNavigationItem
    Signed-off-by: Marco Ambrosini <marcoambrosini@pm.me>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,86 @@
 <template>
 	<Content :class="{'icon-loading': loading}" app-name="vueexample">
 		<AppNavigation>
-			<AppNavigationNew v-if="!loading"
-				:text="t('vueexample', 'New XXXXXX')"
-				:disabled="false"
-				button-id="new-vueexample-button"
-				button-class="icon-add"
-				@click="newButtonAction" />
+			<AppNavigationNew v-if="!loading" :text="t('vueexample', 'New XXXXXX')" :disabled="false"
+				button-id="new-vueexample-button" button-class="icon-add" @click="newButtonAction" />
 			<ul id="app-vueexample-navigation">
-				<AppNavigationItem v-for="item in menu" :key="item.key" :item="item" />
+				<AppNavigationItem icon="icon-user" title="This is a user">
+					<AppNavigationCounter slot="counter" :highlighted="true">
+						99+
+					</AppNavigationCounter>
+					<template slot="actions">
+						<ActionButton icon="icon-edit" @click="alert('Edit')">
+							Edit
+						</ActionButton>
+						<ActionButton icon="icon-delete" @click="alert('Delete')">
+							Delete
+						</ActionButton>
+						<ActionLink icon="icon-external" title="Link" href="https://nextcloud.com" />
+					</template>
+				</AppNavigationItem>
+				<AppNavigationItem  title="This is bullet item">
+					<AppNavigationIconBullet slot="icon" color="0082c9" />
+					<template slot="actions">
+						<ActionButton :disabled="false" icon="icon-edit" @click="alert('Edit')">
+							Edit
+						</ActionButton>
+						<ActionLink :disabled="false" icon="icon-external" title="Link"
+							href="https://nextcloud.com" />
+						<ActionCheckbox :disabled="false">
+							Checkbox
+						</ActionCheckbox>
+						<ActionInput :disabled="false" icon="icon-add">
+							ActionInput
+						</ActionInput>
+						<ActionRouter :disabled="false" icon="icon-add" to="#">
+							ActionRouter
+						</ActionRouter>
+						<ActionText :disabled="false" icon="icon-add">
+							ActionText
+						</ActionText>
+						<ActionTextEditable :disabled="false">
+							ActionTextEditable
+						</ActionTextEditable>
+					</template>
+				</AppNavigationItem>
+				<AppNavigationItem title="Counter and actions"
+					icon="icon-category-enabled">
+					<AppNavigationCounter slot="counter" :highlighted="true">
+						99+
+					</AppNavigationCounter>
+					<template slot="actions">
+						<ActionButton icon="icon-edit" @click="alert('Edit')">
+							Edit
+						</ActionButton>
+						<ActionButton icon="icon-delete" @click="alert('Delete')">
+							Delete
+						</ActionButton>
+						<ActionLink icon="icon-external" title="Link" href="https://nextcloud.com" />
+					</template>
+				</AppNavigationItem>
+				<AppNavigationItem title="Item with children" :allowCollapse="true"
+					icon="icon-folder">
+					<template>
+						<AppNavigationItem title="AppNavigationItemChild1">
+							<AppNavigationIconBullet slot="icon" color="0082c9" />
+						</AppNavigationItem>
+						<AppNavigationItem title="AppNavigationItemChild2" icon="icon-category-enabled" />
+						<AppNavigationItem title="AppNavigationItemChild3" icon="icon-category-enabled" />
+						<AppNavigationItem title="AppNavigationItemChild4" icon="icon-category-enabled" />
+					</template>
+				</AppNavigationItem>
+				<AppNavigationItem title="Loading Item" :loading="true" />
+				<AppNavigationItem title="Event Item" @click="console.log('clicked')"  icon="icon-folder" />
+				<AppNavigationItem title="Editable Item" icon="icon-folder"
+					:editable="true"
+					editPlaceholder="I am a placeholder" />
+				<AppNavigationItem title="Are you sure you want to delete?" :undo="true" />
+				<AppNavigationItem title="First pinned item" icon="icon-category-enabled"
+					:pinned="true" />
+				<AppNavigationItem title="Second pinned item" icon="icon-category-enabled"
+					:pinned="true" />
+				<AppNavigationItem title="Third pinned item" icon="icon-category-enabled"
+					:pinned="true" />
 			</ul>
 			<AppNavigationSettings>
 				Example settings
@@ -20,19 +92,14 @@
 				Toggle sidebar
 			</button>
 		</AppContent>
-		<AppSidebar v-show="show"
-			title="christmas-image-2018-12-25-00:01:12.jpg"
-			subtitle="4,3 MB, last edited 41 days ago"
-			:actions="menu"
-			:starred.sync="starred"
+		<AppSidebar v-show="show" title="christmas-image-2018-12-25-00:01:12.jpg" subtitle="4,3 MB, last edited 41 days ago"
+			:actions="menu" :starred.sync="starred"
 			@close="show=false">
 			<template #action>
 				<button class="primary">
 					Button 1
 				</button>
-				<input id="link-checkbox"
-					name="link-checkbox"
-					class="checkbox link-checkbox"
+				<input id="link-checkbox" name="link-checkbox" class="checkbox link-checkbox"
 					type="checkbox">
 				<label for="link-checkbox" class="link-checkbox-label">Do something</label>
 			</template>
@@ -56,14 +123,25 @@
 </template>
 
 <script>
-import Content from 'nextcloud-vue/dist/Components/Content'
-import AppContent from 'nextcloud-vue/dist/Components/AppContent'
-import AppNavigation from 'nextcloud-vue/dist/Components/AppNavigation'
-import AppNavigationItem from 'nextcloud-vue/dist/Components/AppNavigationItem'
-import AppNavigationNew from 'nextcloud-vue/dist/Components/AppNavigationNew'
-import AppNavigationSettings from 'nextcloud-vue/dist/Components/AppNavigationSettings'
-import AppSidebar from 'nextcloud-vue/dist/Components/AppSidebar'
-import AppSidebarTab from 'nextcloud-vue/dist/Components/AppSidebarTab'
+import {
+	Content,
+	AppContent,
+	AppNavigation,
+	AppNavigationItem,
+	AppNavigationNew,
+	AppNavigationSettings,
+	AppSidebar,
+	AppSidebarTab,
+	AppNavigationCounter,
+	ActionButton,
+	ActionLink,
+	AppNavigationIconBullet,
+	ActionCheckbox,
+	ActionInput,
+	ActionRouter,
+	ActionText,
+	ActionTextEditable
+} from 'nextcloud-vue'
 
 export default {
 	name: 'App',
@@ -75,7 +153,16 @@ export default {
 		AppNavigationNew,
 		AppNavigationSettings,
 		AppSidebar,
-		AppSidebarTab
+		AppSidebarTab,
+		AppNavigationCounter,
+		ActionButton,
+		ActionLink,
+		AppNavigationIconBullet,
+		ActionCheckbox,
+		ActionInput,
+		ActionRouter,
+		ActionText,
+		ActionTextEditable
 	},
 	data: function() {
 		return {
@@ -121,28 +208,7 @@ export default {
 				{
 					id: 'app-category-enabled',
 					classes: [],
-					icon: 'icon-category-enabled',
-					href: '#3',
-					utils: {
-						counter: 123,
-						actions: [
-							{
-								icon: 'icon-delete',
-								text: t('settings', 'Remove group'),
-								action: function() {
-									alert('remove')
-								}
-							},
-							{
-								icon: 'icon-delete',
-								text: t('settings', 'Remove group'),
-								action: function() {
-									alert('remove')
-								}
-							}
-						]
-					},
-					text: t('settings', 'Active apps')
+					href: '#3'
 				},
 				{
 					id: 'app-category-disabled',
